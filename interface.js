7567d4a32b1e30ad07f78a8096b7ac3daf7d965a
Freddie <fredjlawson@gmail.com>
2018-09-06 21:50:14 +0100

    adds dynamic color for energy usage

--- interface.js
+++ interface.js
@@ -7,41 +7,44 @@ $(document).ready(function() {
 
   var thermostat = new Thermostat();
 
-
   updateTemperature();
+  updateEnergyUsageColor();
 
   $('#minus').click(function() {
     thermostat.decrease();
     updateTemperature();
+    updateEnergyUsageColor();
   });
 
   $('#plus').click(function() {
     thermostat.increase();
     updateTemperature();
+    updateEnergyUsageColor();
   });
 
   $('#reset').click(function() {
     thermostat.resetTemperature();
     updateTemperature();
+    updateEnergyUsageColor();
   });
 
   $('#powerSave').click(function() {
     thermostat.powerSavingSwitch();
   });
 
-
-
+  function updateEnergyUsageColor() {
+    if (thermostat.energyUsage() === 'low-usage') {
+      $('#energy').css('background-color', 'green');    
+    } else if (thermostat.energyUsage() === 'medium-usage') {
+      $('#energy').css('background-color', 'black');
+    } else {
+      $('#energy').css('background-color', 'red');
+    }
+  }
+  
   function updateTemperature() {
     $('#temp').text(thermostat.temperature);
   };
-
-
-
-
-
-
-
-
 
 
 });
